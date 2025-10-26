66b2c67ff0cd00704a8bd3607213536a0885c208
jyotishankar04 <patrajyotishankar@gmail.com>
2025-10-26 22:23:47 +0530

    inti

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom"
+import Landing from "@/pages"
+
+const App = () => {
+  return (
+    <Routes>  
+      <Route path="/" element={< Landing/>} />
+    </Routes> 
+  )
+}
+
+export default App
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "@/components/custom/landing/hero-section"
+
+const Landing = () => {
+    return (
+        <div className="min-h-screen w-full relative">
+            {/* Your Content/Components */}
+            <HeroSection />
+        </div>
+    )
+}
+
+export default Landing
